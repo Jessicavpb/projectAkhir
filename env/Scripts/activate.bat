896620b20d6cead71c0597c8ce60a37356aaeb19
Jessicavpb <jvpb29@gmail.com>
2021-06-17 23:31:20 +0700

    projectAkhir

---
+++ env/Scripts/activate.bat
@@ -0,0 +1,42 @@
+@echo off
+
+rem This file is UTF-8 encoded, so we need to update the current code page while executing it
+for /f "tokens=2 delims=:." %%a in ('"%SystemRoot%\System32\chcp.com"') do (
+    set _OLD_CODEPAGE=%%a
+)
+if defined _OLD_CODEPAGE (
+    "%SystemRoot%\System32\chcp.com" 65001 > nul
+)
+
+set VIRTUAL_ENV=C:\repo\projectAkhir\env
+
+if not defined PROMPT set PROMPT=$P$G
+
+if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
+if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
+
+set _OLD_VIRTUAL_PROMPT=%PROMPT%
+set PROMPT=(env) %PROMPT%
+
+if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
+set PYTHONHOME=
+
+if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
+if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
+
+set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+
+:END
+if defined _OLD_CODEPAGE (
+    "%SystemRoot%\System32\chcp.com" %_OLD_CODEPAGE% > nul
+    set _OLD_CODEPAGE=
+)
+set "DB_URL=postgresql://postgres:JESSICA@localhost/noteapp"
+
+set "DB_URL=postgresql://postgres:JESSICA@localhost/contactapp"
+
+set "DB_USERNAME=postgres"
+set "DB_PASSWORD=JESSICA"
+set "FLASK_APP=application.py"
+set "FLASK_DEBUG=1"
+set "DB_URL=postgresql://postgres:JESSICA@localhost/lecturexikom"
